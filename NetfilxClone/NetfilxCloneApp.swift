8b213d0414379a39451a1b49a725a55f17252a57
iamudesharma <udesh2568@gmail.com>
2022-08-14 12:53:53 +0530

    Initial Commit

---
+++ NetfilxClone/NetfilxCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetfilxCloneApp.swift
+//  NetfilxClone
+//
+//  Created by udesh sharma on 14/08/22.
+//
+
+import SwiftUI
+
+@main
+struct NetfilxCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
